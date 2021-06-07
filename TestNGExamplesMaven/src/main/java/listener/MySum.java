6bcaf50ea86ee389dbca38a692b5b318e39257a3
jem <jem@10.10.209.121>
2021-06-07 16:48:52 -0400

    Second Commit Test for sum Class

---
+++ TestNGExamplesMaven/src/main/java/listener/MySum.java
@@ -0,0 +1,15 @@
+package listener;
+
+public class MySum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=0,b=5;
+		
+		int c= a+b;
+		
+		System.out.println("Sum is "+c);
+
+	}
+
+}
